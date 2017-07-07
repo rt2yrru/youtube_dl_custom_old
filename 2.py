ae3982de0b68553b9259ec4c3602fc5a4a31fa60
siddht1 <dhar.75.ap@gmail.com>
2017-07-07 15:17:59 +0000

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals 
+import youtube_dl
+ydl_opts = {}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+meta = ydl.extract_info( 'https://www.youtube.com/watch?v=9bZkp7q19f0', download=False) #download set to no i.e false
+  print 'upload date : %s' %(meta['upload_date'])
+  print 'uploader : %s' %(meta['uploader'])
+  print 'views : %d' %(meta['view_count']) 
+  print 'likes : %d' %(meta['like_count']) 
+  print 'dislikes : %d' %(meta['dislike_count']) 
+  print 'id : %s' %(meta['id'])
+  print 'format : %s' %(meta['format'])
+  print 'duration : %s' %(meta['duration'])
+  print 'title : %s' %(meta['title'])
+  print 'description : %s' %(meta['description']) 

2d889257df6a6880dd5513a18a031da1fa09f7b8
rt1yrru <dhar.75.ap@gmail.com>
2017-07-11 23:44:51 +0530

    fixed

--- test_2.py
+++ test_2.py
@@ -1,8 +1,10 @@
 from __future__ import unicode_literals 
-import youtube_dl
+import youtube_dl,json
 ydl_opts = {}
 _url_='https://www.youtube.com/watch?v=2YuLhvdg5xc' #provide the url 
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
   meta = ydl.extract_info( _url_, download=False) #download set to no i.e false
+  _json=json.dumps(meta)
+  print(_json)
     
  

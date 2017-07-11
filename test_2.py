b8268cd3845b57609d7ca3ec0e2dcce6a7f724f1
rt1yrru <dhar.75.ap@gmail.com>
2017-07-11 23:07:39 +0530

    added new test,gpg key added

---
+++ test_2.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals 
+import youtube_dl
+ydl_opts = {}
+_url_='https://www.youtube.com/watch?v=2YuLhvdg5xc' #provide the url 
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+meta = ydl.extract_info( _url_, download=False) #download set to no i.e false
+  print 'upload date : %s' %(meta['upload_date'])
+  print 'uploader : %s' %(meta['uploader'])
+  print 'views : %d' %(meta['view_count']) 
+  print 'likes : %d' %(meta['like_count']) 
+  print 'dislikes : %d' %(meta['dislike_count']) 
+  print 'id : %s' %(meta['id'])
+  print 'format : %s' %(meta['format'])
+  print 'duration : %s' %(meta['duration'])
+  print 'title : %s' %(meta['title'])
+  print 'description : %s' %(meta['description']) 
+ 

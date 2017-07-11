0329f22c8050c09ec8974bb857acc1fad675559f
rt1yrru <dhar.75.ap@gmail.com>
2017-07-11 23:16:22 +0530

    fixed test_2.py

--- test_2.py
+++ test_2.py
@@ -3,15 +3,15 @@ import youtube_dl
 ydl_opts = {}
 _url_='https://www.youtube.com/watch?v=2YuLhvdg5xc' #provide the url 
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-meta = ydl.extract_info( _url_, download=False) #download set to no i.e false
-  print 'upload date : %s' %(meta['upload_date'])
-  print 'uploader : %s' %(meta['uploader'])
-  print 'views : %d' %(meta['view_count']) 
-  print 'likes : %d' %(meta['like_count']) 
-  print 'dislikes : %d' %(meta['dislike_count']) 
-  print 'id : %s' %(meta['id'])
-  print 'format : %s' %(meta['format'])
-  print 'duration : %s' %(meta['duration'])
-  print 'title : %s' %(meta['title'])
-  print 'description : %s' %(meta['description']) 
+  meta = ydl.extract_info( _url_, download=False) #download set to no i.e false
+     print 'upload date : %s' %(meta['upload_date'])
+     print 'uploader : %s' %(meta['uploader'])
+     print 'views : %d' %(meta['view_count']) 
+     print 'likes : %d' %(meta['like_count']) 
+     print 'dislikes : %d' %(meta['dislike_count']) 
+     print 'id : %s' %(meta['id'])
+     print 'format : %s' %(meta['format'])
+     print 'duration : %s' %(meta['duration'])
+     print 'title : %s' %(meta['title'])
+     print 'description : %s' %(meta['description']) 
  
